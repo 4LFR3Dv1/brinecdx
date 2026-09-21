f4aca40920d7450709ab870caf278715c6ea6b78
Renan Melo <byrenanmelo@gmail.com>
2026-09-20 21:32:49 -0300

    R3.5 P5: export routed model preset

--- codex-rs/core/src/lib.rs
+++ codex-rs/core/src/lib.rs
@@ -151,6 +151,7 @@ pub(crate) mod windows_sandbox_read_grants;
 pub use thread_manager::ForkSnapshot;
 pub use thread_manager::InternalSessionParent;
 pub use thread_manager::NewThread;
+pub use thread_manager::RoutedModelPreset;
 pub use thread_manager::StartThreadOptions;
 pub use thread_manager::ThreadManager;
 pub use thread_manager::ThreadShutdownReport;
